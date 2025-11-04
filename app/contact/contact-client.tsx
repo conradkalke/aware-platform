0337370aceeeab71eaa8a794f919a8c487b1a317
Conrad Kalke <conradkalke@Conrads-MacBook-Air.local>
2025-11-04 14:47:33 -0500

    Remove quotation marks from lab registration document name

--- app/contact/contact-client.tsx
+++ app/contact/contact-client.tsx
@@ -153,7 +153,7 @@ export function ContactPageClient() {
                       download
                       className="underline font-medium hover:text-rose-900"
                     >
-                      "AWARE Lab Registration Document"
+                      AWARE Lab Registration Document
                     </a>
                     {' '}and email it to{' '}
                     <a 
